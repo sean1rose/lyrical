b7e545a254c6407b39ee4c630a69e640640f97ff
Sean Rose <sean1rose@gmail.com>
2017-04-03 18:51:04 -0700

    begin apollo setup

--- client/index.js
+++ client/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// AC is interacting w/ our graphql on backend, makes requests for data then stores locally when response comes back
+import ApolloClient from 'apollo-client';
+
+// is like the glue b/w react and apollo
+  // pass client as a prop
+import { ApolloProvider } from 'react-apollo';
+
+
+// create new instance of ApolloClient, and pass it to ApolloProvider
+  // assumes that graphql is available on /graphql route
+const client = new ApolloClient({});
 
 const Root = () => {
-  return <div>Lyrical</div>
+  return (
+    <ApolloProvider client={client}>
+      <div>Lyrical</div>
+    </ApolloProvider>
+  )
 };
 
 ReactDOM.render(
