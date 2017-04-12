c7c7024e0d18080256e28764bb6784d7a2e902a0
Sean Rose <sean1rose@gmail.com>
2017-04-12 00:50:48 -0700

    start react router hook up

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 
+// helper to glue together a query and a react component...
+import { graphql } from 'react-apollo';
+// query...
+import fetchSong from '../queries/fetchSong';
+
 class SongDetail extends Component {
   render() {
+    console.log(this.props);
     return (
       <div>
         <h3>Song Detail</h3>
@@ -10,4 +16,8 @@ class SongDetail extends Component {
   }
 }
 
-export default SongDetail;
+// rendering a component wrapped by graphql helper, which knows props coming down into SongDetail component
+  // options takes props intended for SongDetail, provides to the query when it's made (provides the id)
+export default graphql(fetchSong, {
+  options: (props) => { return { variables: { id: props.params.id } } }
+})(SongDetail);
