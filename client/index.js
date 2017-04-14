7663dcc6816db26f748859ee8d82881296986bc0
Sean Rose <sean1rose@gmail.com>
2017-04-14 02:04:08 -0700

    set up caching system instead of refetch query

--- client/index.js
+++ client/index.js
@@ -19,8 +19,17 @@ import SongDetail from './components/SongDetail';
 
 // create new instance of ApolloClient, and pass it to ApolloProvider
   // assumes that graphql is available on /graphql route
-const client = new ApolloClient({});
+const client = new ApolloClient({
+  dataIdFromObject: o => o.id
+});
 // ^ **STORE**
+
+  // http://dev.apollodata.com/react/cache-updates.html
+  // Caching system... (rather than using the follow up refetch query)
+  // dataIdFromObject -> helps apollo identify its data - take every piece of data and run it thru the function to identify each piece of data
+    // look at every record and use the 'id' field to identify each record
+      // so now need to make sure to ask for id every time we make a graphql query
+      // **now apollo knows when a record is updated, don't need to manually ask it to refresh and refetch; it automatically re-renders
 
 const Root = () => {
   return (
