53c5a8b6e6c93abceb6d129afcec412513176777
Sean Rose <sean1rose@gmail.com>
2017-04-14 02:07:16 -0700

    set up thumbs up icon

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 
 class LyricList extends Component {
+  onLike(id) {
+    console.log('ID! - ', id);
+  }
+  
   renderLyrics() {
     return this.props.lyrics.map(({ id, content }) => {
       // for every lyric list item -> render a chunk of jsx
       return (
         <li key={id} className="collection-item">
           {content}
+          <i className="material-icons" onClick={() => this.onLike(id)}>thumb_up</i>
         </li>
       );
     });
